c9905dcc4aa8300a537809da178a5a57ec2e98c9
Dennis Sell <sell.dennis@gmail.com>
2013-05-07 14:03:46 -0400

    fixed FileNames to return expect char, and correct file locations

--- hnn-master/LetterMatcher.hs
+++ hnn-master/LetterMatcher.hs
@@ -11,15 +11,19 @@ data Person = D
             | DR
 
 
-getFileNames :: Person -> Integer -> [String]
-getFileNames p s  = [name : letter : set : ".bmp" |
+getFileNames :: Person -> Integer -> [(Char,String)]
+getFileNames p s  = [(letter,  name ++ (letter : set : ".bmp")) |
                             name <- names p, set <- sets, letter <- chars]
     where
         sets = map (chr . fromIntegral . (+48)) [1..s]
-
         chars = ['a','b','c','d','e','f','g','h','i','j','k','l','m']
              ++ ['n','o','p','q','r','s','t','u','v','w','x','y','z']
-        names D = ['d']
-        names R = ['r']
-        names DR = ['d', 'r']
+        names D = ["Dennis/d"]
+        names R = ["Richie/r"]
+        names DR = ["Dennis/d", "Richie/r"]
+
+
+getSamples :: [String] -> [IO(Maybe [Word8])]
+getSamples xs = map (unpackB . getByteStringFromFile) xs
+
 
--- hnn-master/TODO.md
+++
@@ -1,5 +0,0 @@
-* Implement cross validation and propose it as a primitive backprop function
-* Implement backprop-with-momentum
-* Implement conjugate gradient backprop
-* Investigate rprop
-* Write hnn-opencl (and/or similar backends) for better performances
