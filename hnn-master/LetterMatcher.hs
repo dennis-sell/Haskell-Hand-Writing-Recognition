8184df20b6806767b640f833c3a3a156f01e758b
Dennis Sell <sell.dennis@gmail.com>
2013-05-07 23:03:57 -0400

    Prettified LetterMatcher code

--- hnn-master/LetterMatcher.hs
+++ hnn-master/LetterMatcher.hs
@@ -6,8 +6,8 @@ import Data.Char
 import Data.String
 --import Data.ByteString
 import Data.Word
-import AI.HNN.FF.Network
-import Numeric.LinearAlgebra
+import AI.HNN.FF.Network 
+{- import Numeric.LinearAlgebra -}
 
 data Person = D
             | R
@@ -21,10 +21,16 @@ stringToPerson s
             | s == "DR" = Just DR
             | otherwise = Nothing
 
+mapBoth :: (a -> c) -> (b -> d) -> (a,b) -> (c,d)
+mapBoth f g (fir,sec) = (f fir, g sec) 
+
 chars = ['a'..'z']
 
 sets :: Integer -> String
 sets i = map (chr . fromIntegral . (+48)) [1..i]
+
+
+-------------- Letter Recognition --------------------------------------------
 
 getFileNames :: Person -> Integer -> [(String, Char)]
 getFileNames p s  = [(name ++ (letter : set : ".bmp"), letter) |
@@ -34,33 +40,35 @@ getFileNames p s  = [(name ++ (letter : set : ".bmp"), letter) |
         names R = ["Richie/r"]
         names DR = ["Dennis/d", "Richie/r"]
 
+charToVector :: Char -> [Double]
+charToVector c = (replicate (numLetter - 1) 0) ++ [1]
+                         ++ (replicate (26 - numLetter) 0)
+    where
+        numLetter = Data.Char.ord c - 96 
+
+getSamples :: Person -> Integer -> [(IO(Maybe [Double]), [Double] )]
+getSamples p i = map (mapBoth getSample charToVector) files
+    where files = getFileNames p i
+
+
+-------------- Style Recognition ---------------------------------------------
+
 getFileNamesStyle :: Integer -> [(String, Person)]
 getFileNamesStyle s  = [("Dennis/d" ++ (letter : set : ".bmp"), D) |
                             set <- sets s, letter <- chars] ++ 
                        [("Richie/r" ++ (letter : set : ".bmp"), R) |
                             set <- sets s, letter <- chars]
 
-mapBoth :: (a -> c) -> (b -> d) -> (a,b) -> (c,d)
-mapBoth f g (fir,sec) = (f fir, g sec) 
-
-charToVector :: Char -> [Double]
-charToVector c = (replicate (numLetter - 1) 0) ++ [1] ++ (replicate (26 - numLetter) 0)
-    where
-        numLetter = Data.Char.ord c - 96 
-
 personToVector :: Person -> [Double]
 personToVector D = [1,0]
 personToVector R = [0,1]
 personToVector _ = [0,0]
 
-getSamples :: Person -> Integer -> [(IO(Maybe [Double]), [Double] )]
-getSamples p i = map (mapBoth getSample charToVector) files
-    where files = getFileNames p i
-
 getStyleSamples :: Integer -> [(IO(Maybe [Double]), [Double] )]
 getStyleSamples i = map (mapBoth getSample personToVector) files
     where files = getFileNamesStyle i
 
+------------------------------------------------------------------------------
 
 main :: IO ()
 main = do
@@ -70,7 +78,7 @@ main = do
     tests <- testsGetLoop
     putStrLn "Creating Neural Network"
     --n <- createNetwork 256 [2560] 26
-    putStrLn "Done"
+ l  putStrLn "Done"
     where 
       personGetLoop :: IO Person
       personGetLoop = (fmap stringToPerson getLine) >>= \ input ->
@@ -80,7 +88,7 @@ main = do
       testsGetLoop :: IO Integer
       testsGetLoop = (fmap integerOfString getLine) >>= \ input ->
         case input of
-          Just num -> if num <= 4 && num >= 1 then return num else testsGetLoop  
+          Just n -> if n <= 4 && n >= 1 then return n else testsGetLoop  
           Nothing -> putStrLn "Is not a valid test number" >> testsGetLoop
       integerOfString :: String -> Maybe Integer
       integerOfString s = case head s of
